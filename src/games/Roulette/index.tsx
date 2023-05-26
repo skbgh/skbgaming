b3b3dfb4df1f900b932eb3c2a0e9e11b97ebdb5a
skbgh <121875497+skbgh@users.noreply.github.com>
2023-05-26 12:24:35 +0800

    Update index.tsx

--- src/games/Roulette/index.tsx
+++ src/games/Roulette/index.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import image from './image.png'
 
 export default {
-  name: 'Roulette',
-  short_name: 'roulette',
+  name: 'SKB Roulette',
+  short_name: 'skbroulette',
   description: '',
-  creator: 'DwRFGbjKbsEhUMe5at3qWvH7i8dAJyhhwdnFoZMnLVRV',
+  creator: '2qjSyMe8Ag57rRTkSRzPrHc2zgS6DmpzrLe7KqE6gL36',
   image,
   theme_color: '#59ff5f',
   app: React.lazy(() => import('./App')),
