b59db76dfac2366c187b15c2bb619ddff4217cb0
skbgh <121875497+skbgh@users.noreply.github.com>
2023-06-08 22:09:33 +0800

    Update index.tsx

--- src/games/HiLo/index.tsx
+++ src/games/HiLo/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import image from './image.png'
 
 export default {
-  name: 'HiLo',
-  short_name: 'hilo',
+  name: 'SKB HiLo',
+  short_name: 'skbhilo',
   description: '',
   creator: '7rdwG1fmii5PmbKuu78sKv1bH96ksTtmqnV6M8ftDnUs',
   image,
